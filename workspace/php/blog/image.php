0001b1c4c710b54713af9162619aecbdd843adc2
lin-chiang <lin-chiang@yy.local>
2016-12-05 16:15:18 +0800

    レスポンシブのサンプル追加

--- workspace/php/blog/image.php
+++ workspace/php/blog/image.php
@@ -2,7 +2,7 @@
 	require 'utils.php';
 	if (isset($_GET['p'])){
 		$profile = get_profile();
-		$image_type = "image/jpeg";
+		$image_type = "image/jpeg";//jpegに決め打ちする。ただ、ブラウザが頑張ればgifが帰ってくる。
 		$image_path = $profile['image_path'];
 		$file_size = filesize($image_path);
 	}else {
@@ -13,8 +13,8 @@
 		$image_path = $post['image_path'];
 		$file_size = filesize($image_path);
 	}
-	header("Content-Length: ${file_size}");
-	header("Content-Type: ${image_type}");
+	header("Content-Length: ${file_size}");//容量サイズ
+	header("Content-Type: ${image_type}");//コンテントタイプタイプは教えてあげる。拡張子、種類、表示したいファイルの種類によって変える。HTMLのときは意識しなくていいが、画面に見えない情報までくれる。
 	echo file_get_contents($image_path);
-	// headerは共通なので括弧の外に出した。画像PHP?pになれば、プロフィール画像が表示される。そこから画像を取ってくるGET。image_typeで特定の拡張子だけ判別する。
+	// headerは共通なので括弧の外に出した。画像PHP?pになれば、プロフィール画像が表示される。そこから画像を取ってくるGET。image_typeが何なのかを特定の拡張子だけ判別する。
 ?>
